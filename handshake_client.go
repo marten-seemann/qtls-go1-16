marten-seemann/qtls

add callbacks to store and restore app data along a session state

handshake_client.go
@@ -12,7 +12,6 @@ import (
 	"crypto/rsa"
 	"crypto/subtle"
 	"crypto/x509"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
@@ -20,7 +19,11 @@ import (
 	"strings"
 	"sync/atomic"
 	"time"
+
+	"golang.org/x/crypto/cryptobyte"
 )
+
+const clientSessionStateVersion = 1
 
 type clientHandshakeState struct {
 	c            *Conn
@@ -258,6 +261,33 @@ func (c *Conn) clientHandshake() (err error) {
 	return nil
 }
 
+// extract the app data saved in the session.nonce,
+// and set the session.nonce to the actual nonce value
+func (c *Conn) decodeSessionState(session *ClientSessionState) (uint32 /* max early data */, []byte /* app data */, bool /* ok */) {
+	s := cryptobyte.String(session.nonce)
+	var version uint16
+	if !s.ReadUint16(&version) {
+		return 0, nil, false
+	}
+	if version != clientSessionStateVersion {
+		return 0, nil, false
+	}
+	var maxEarlyData uint32
+	if !s.ReadUint32(&maxEarlyData) {
+		return 0, nil, false
+	}
+	var appData []byte
+	if !readUint16LengthPrefixed(&s, &appData) {
+		return 0, nil, false
+	}
+	var nonce []byte
+	if !readUint16LengthPrefixed(&s, &nonce) {
+		return 0, nil, false
+	}
+	session.nonce = nonce
+	return maxEarlyData, appData, true
+}
+
 func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 	session *ClientSessionState, earlySecret, binderKey []byte) {
 	if c.config.SessionTicketsDisabled || c.config.ClientSessionCache == nil {
@@ -284,6 +314,16 @@ func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 	session, ok := c.config.ClientSessionCache.Get(cacheKey)
 	if !ok || session == nil {
 		return cacheKey, nil, nil, nil
+	}
+	var appData []byte
+	var maxEarlyData uint32
+	if session.vers == VersionTLS13 {
+		var ok bool
+		maxEarlyData, appData, ok = c.decodeSessionState(session)
+		if !ok { // delete it, if parsing failed
+			c.config.ClientSessionCache.Put(cacheKey, nil)
+			return cacheKey, nil, nil, nil
+		}
 	}
 
 	// Check that version used for the previous session is still valid.
@@ -327,14 +367,6 @@ func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 		hello.sessionTicket = session.sessionTicket
 		return
 	}
-
-	// In TLS 1.3, we abuse the nonce field to save the max_early_data_size.
-	// See Conn.handleNewSessionTicket for an explanation of this hack.
-	if len(session.nonce) < 4 {
-		return cacheKey, nil, nil, nil
-	}
-	maxEarlyData := binary.BigEndian.Uint32(session.nonce[:4])
-	session.nonce = session.nonce[4:]
 
 	// Check that the session ticket is not expired.
 	if c.config.time().After(session.useBy) {
@@ -380,6 +412,9 @@ func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 	pskBinders := [][]byte{cipherSuite.finishedHash(binderKey, transcript)}
 	hello.updateBinders(pskBinders)
 
+	if session.vers == VersionTLS13 && c.config.SetAppDataFromSessionState != nil {
+		c.config.SetAppDataFromSessionState(appData)
+	}
 	return
 }
 

handshake_client_test.go
@@ -868,12 +868,14 @@ func TestClientKeyUpdate(t *testing.T) {
 }
 
 func TestResumption(t *testing.T) {
-	t.Run("TLSv12", func(t *testing.T) { testResumption(t, VersionTLS12) })
-	t.Run("TLSv13", func(t *testing.T) { testResumption(t, VersionTLS13) })
-	t.Run("TLSv13 with 0-RTT", testResumption0RTT)
+	t.Run("TLSv12", func(t *testing.T) { testResumption(t, VersionTLS12, false) })
+	t.Run("TLSv13", func(t *testing.T) { testResumption(t, VersionTLS13, false) })
+	t.Run("TLSv13, saving app data", func(t *testing.T) { testResumption(t, VersionTLS13, true) })
+	t.Run("TLSv13, with 0-RTT", func(t *testing.T) { testResumption0RTT(t, false) })
+	t.Run("TLSv13, with 0-RTT, saving app data", func(t *testing.T) { testResumption0RTT(t, true) })
 }
 
-func testResumption(t *testing.T, version uint16) {
+func testResumption(t *testing.T, version uint16, saveAppData bool) {
 	if testing.Short() {
 		t.Skip("skipping in -short mode")
 	}
@@ -891,12 +893,17 @@ func testResumption(t *testing.T, version uint16) {
 	rootCAs := x509.NewCertPool()
 	rootCAs.AddCert(issuer)
 
+	var restoredAppData []byte
 	clientConfig := &Config{
 		MaxVersion:         version,
 		CipherSuites:       []uint16{TLS_RSA_WITH_RC4_128_SHA},
 		ClientSessionCache: NewLRUClientSessionCache(32),
 		RootCAs:            rootCAs,
 		ServerName:         "example.golang",
+	}
+	if saveAppData {
+		clientConfig.GetAppDataForSessionState = func() []byte { return []byte("foobar") }
+		clientConfig.SetAppDataFromSessionState = func(data []byte) { restoredAppData = data }
 	}
 
 	testResumeState := func(test string, didResume bool) {
@@ -909,6 +916,12 @@ func testResumption(t *testing.T, version uint16) {
 		}
 		if didResume && (hs.PeerCertificates == nil || hs.VerifiedChains == nil) {
 			t.Fatalf("expected non-nil certificates after resumption. Got peerCertificates: %#v, verifiedCertificates: %#v", hs.PeerCertificates, hs.VerifiedChains)
+		}
+		if didResume && saveAppData {
+			if !bytes.Equal(restoredAppData, []byte("foobar")) {
+				t.Fatalf("Expected to restore app data saved with the session state. Got: %#v", restoredAppData)
+			}
+			restoredAppData = nil
 		}
 	}
 
@@ -1000,7 +1013,7 @@ func testResumption(t *testing.T, version uint16) {
 	testResumeState("WithoutSessionCache", false)
 }
 
-func testResumption0RTT(t *testing.T) {
+func testResumption0RTT(t *testing.T, saveAppData bool) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -1012,6 +1025,11 @@ func testResumption0RTT(t *testing.T) {
 	clientConfig := testConfig.Clone()
 	clientConfig.Enable0RTT = true
 	clientConfig.ClientSessionCache = cache
+	var restoredAppData []byte
+	if saveAppData {
+		clientConfig.GetAppDataForSessionState = func() []byte { return []byte("foobar") }
+		clientConfig.SetAppDataFromSessionState = func(data []byte) { restoredAppData = data }
+	}
 
 	// check that the ticket is deleted when 0-RTT is used
 	var state *ClientSessionState
@@ -1062,6 +1080,9 @@ func testResumption0RTT(t *testing.T) {
 	}
 	if hs.Used0RTT {
 		t.Fatal("should not have used 0-RTT during the second handshake")
+	}
+	if saveAppData && !bytes.Equal(restoredAppData, []byte("foobar")) {
+		t.Fatalf("expected app data to be restored. Got: %#v", restoredAppData)
 	}
 }
 

marten-seemann/qtls-go1-16

handshake_client.go
@@ -13,7 +13,6 @@ import (
 	"crypto/rsa"
 	"crypto/subtle"
 	"crypto/x509"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"hash"
@@ -22,7 +21,11 @@ import (
 	"strings"
 	"sync/atomic"
 	"time"
+
+	"golang.org/x/crypto/cryptobyte"
 )
+
+const clientSessionStateVersion = 1
 
 type clientHandshakeState struct {
 	c            *Conn
@@ -287,6 +290,33 @@ func (c *Conn) clientHandshake(ctx context.Context) (err error) {
 	return nil
 }
 
+// extract the app data saved in the session.nonce,
+// and set the session.nonce to the actual nonce value
+func (c *Conn) decodeSessionState(session *clientSessionState) (uint32 /* max early data */, []byte /* app data */, bool /* ok */) {
+	s := cryptobyte.String(session.nonce)
+	var version uint16
+	if !s.ReadUint16(&version) {
+		return 0, nil, false
+	}
+	if version != clientSessionStateVersion {
+		return 0, nil, false
+	}
+	var maxEarlyData uint32
+	if !s.ReadUint32(&maxEarlyData) {
+		return 0, nil, false
+	}
+	var appData []byte
+	if !readUint16LengthPrefixed(&s, &appData) {
+		return 0, nil, false
+	}
+	var nonce []byte
+	if !readUint16LengthPrefixed(&s, &nonce) {
+		return 0, nil, false
+	}
+	session.nonce = nonce
+	return maxEarlyData, appData, true
+}
+
 func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 	session *clientSessionState, earlySecret, binderKey []byte) {
 	if c.config.SessionTicketsDisabled || c.config.ClientSessionCache == nil {
@@ -315,6 +345,17 @@ func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 		return cacheKey, nil, nil, nil
 	}
 	session = fromClientSessionState(sess)
+
+	var appData []byte
+	var maxEarlyData uint32
+	if session.vers == VersionTLS13 {
+		var ok bool
+		maxEarlyData, appData, ok = c.decodeSessionState(session)
+		if !ok { // delete it, if parsing failed
+			c.config.ClientSessionCache.Put(cacheKey, nil)
+			return cacheKey, nil, nil, nil
+		}
+	}
 
 	// Check that version used for the previous session is still valid.
 	versOk := false
@@ -357,14 +398,6 @@ func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 		hello.sessionTicket = session.sessionTicket
 		return
 	}
-
-	// In TLS 1.3, we abuse the nonce field to save the max_early_data_size.
-	// See Conn.handleNewSessionTicket for an explanation of this hack.
-	if len(session.nonce) < 4 {
-		return cacheKey, nil, nil, nil
-	}
-	maxEarlyData := binary.BigEndian.Uint32(session.nonce[:4])
-	session.nonce = session.nonce[4:]
 
 	// Check that the session ticket is not expired.
 	if c.config.time().After(session.useBy) {
@@ -412,6 +445,9 @@ func (c *Conn) loadSession(hello *clientHelloMsg) (cacheKey string,
 	pskBinders := [][]byte{cipherSuite.finishedHash(binderKey, transcript)}
 	hello.updateBinders(pskBinders)
 
+	if session.vers == VersionTLS13 && c.extraConfig != nil && c.extraConfig.SetAppDataFromSessionState != nil {
+		c.extraConfig.SetAppDataFromSessionState(appData)
+	}
 	return
 }
 

handshake_client_test.go
@@ -871,12 +871,14 @@ func TestClientKeyUpdate(t *testing.T) {
 }
 
 func TestResumption(t *testing.T) {
-	t.Run("TLSv12", func(t *testing.T) { testResumption(t, VersionTLS12) })
-	t.Run("TLSv13", func(t *testing.T) { testResumption(t, VersionTLS13) })
-	t.Run("TLSv13 with 0-RTT", testResumption0RTT)
+	t.Run("TLSv12", func(t *testing.T) { testResumption(t, VersionTLS12, false) })
+	t.Run("TLSv13", func(t *testing.T) { testResumption(t, VersionTLS13, false) })
+	t.Run("TLSv13, saving app data", func(t *testing.T) { testResumption(t, VersionTLS13, true) })
+	t.Run("TLSv13, with 0-RTT", func(t *testing.T) { testResumption0RTT(t, false) })
+	t.Run("TLSv13, with 0-RTT, saving app data", func(t *testing.T) { testResumption0RTT(t, true) })
 }
 
-func testResumption(t *testing.T, version uint16) {
+func testResumption(t *testing.T, version uint16, saveAppData bool) {
 	if testing.Short() {
 		t.Skip("skipping in -short mode")
 	}
@@ -894,6 +896,7 @@ func testResumption(t *testing.T, version uint16) {
 	rootCAs := x509.NewCertPool()
 	rootCAs.AddCert(issuer)
 
+	var restoredAppData []byte
 	clientConfig := &Config{
 		MaxVersion:         version,
 		CipherSuites:       []uint16{TLS_RSA_WITH_RC4_128_SHA},
@@ -901,9 +904,14 @@ func testResumption(t *testing.T, version uint16) {
 		RootCAs:            rootCAs,
 		ServerName:         "example.golang",
 	}
+	clientExtraConfig := &ExtraConfig{}
+	if saveAppData {
+		clientExtraConfig.GetAppDataForSessionState = func() []byte { return []byte("foobar") }
+		clientExtraConfig.SetAppDataFromSessionState = func(data []byte) { restoredAppData = data }
+	}
 
 	testResumeState := func(test string, didResume bool) {
-		_, hs, err := testHandshake(t, clientConfig, serverConfig)
+		_, hs, err := testHandshakeWithExtraConfig(t, clientConfig, clientExtraConfig, serverConfig, nil)
 		if err != nil {
 			t.Fatalf("%s: handshake failed: %s", test, err)
 		}
@@ -915,6 +923,12 @@ func testResumption(t *testing.T, version uint16) {
 		}
 		if got, want := hs.ServerName, clientConfig.ServerName; got != want {
 			t.Errorf("%s: server name %s, want %s", test, got, want)
+		}
+		if didResume && saveAppData {
+			if !bytes.Equal(restoredAppData, []byte("foobar")) {
+				t.Fatalf("Expected to restore app data saved with the session state. Got: %#v", restoredAppData)
+			}
+			restoredAppData = nil
 		}
 	}
 
@@ -1054,7 +1068,7 @@ func testResumption(t *testing.T, version uint16) {
 	testResumeState("WithoutSessionCache", false)
 }
 
-func testResumption0RTT(t *testing.T) {
+func testResumption0RTT(t *testing.T, saveAppData bool) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -1068,6 +1082,11 @@ func testResumption0RTT(t *testing.T) {
 	clientConfig := testConfig.Clone()
 	clientConfig.ClientSessionCache = cache
 	clientExtraConfig := &ExtraConfig{Enable0RTT: true}
+	var restoredAppData []byte
+	if saveAppData {
+		clientExtraConfig.GetAppDataForSessionState = func() []byte { return []byte("foobar") }
+		clientExtraConfig.SetAppDataFromSessionState = func(data []byte) { restoredAppData = data }
+	}
 
 	// check that the ticket is deleted when 0-RTT is used
 	var state *ClientSessionState
@@ -1118,6 +1137,9 @@ func testResumption0RTT(t *testing.T) {
 	}
 	if hs.Used0RTT {
 		t.Fatal("should not have used 0-RTT during the second handshake")
+	}
+	if saveAppData && !bytes.Equal(restoredAppData, []byte("foobar")) {
+		t.Fatalf("expected app data to be restored. Got: %#v", restoredAppData)
 	}
 }
 