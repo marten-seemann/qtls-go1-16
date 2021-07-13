marten-seemann/qtls

add a config option to enforce selection of an application protocol

handshake_server_test.go
@@ -927,6 +927,30 @@ func TestHandshakeServerALPN(t *testing.T) {
 	runServerTestTLS13(t, test)
 }
 
+func TestHandshakeServerEnforceALPNMatch(t *testing.T) {
+	clientConn, serverConn := localPipe(t)
+	client := Client(clientConn, testConfig)
+
+	cErrChan := make(chan error)
+	go func() {
+		cErrChan <- client.Handshake()
+	}()
+
+	config := testConfig.Clone()
+	config.NextProtos = []string{"proto1"}
+	config.EnforceNextProtoSelection = true
+
+	server := Server(serverConn, config)
+	err := server.Handshake()
+	if err == nil || err.Error() != "ALPN negotiation failed" {
+		t.Fatalf("Expected APLN negotiation to fail, got %s", err)
+	}
+	cErr := <-cErrChan
+	if cErr == nil || !strings.Contains(cErr.Error(), "no application protocol") {
+		t.Fatalf("Expect 'no_application_protocol' error, got %s", cErr)
+	}
+}
+
 func TestHandshakeServerALPNNoMatch(t *testing.T) {
 	config := testConfig.Clone()
 	config.NextProtos = []string{"proto3"}

marten-seemann/qtls-go1-16

handshake_server_test.go
@@ -908,6 +908,32 @@ func TestHandshakeServerALPN(t *testing.T) {
 	runServerTestTLS13(t, test)
 }
 
+func TestHandshakeServerEnforceALPNMatch(t *testing.T) {
+	clientConn, serverConn := localPipe(t)
+	serverConfig := testConfig.Clone()
+	serverConfig.NextProtos = []string{"proto1", "proto2"}
+	client := Client(clientConn, serverConfig, nil)
+
+	cErrChan := make(chan error)
+	go func() {
+		cErrChan <- client.Handshake()
+	}()
+
+	config := testConfig.Clone()
+	config.NextProtos = []string{"proto3"}
+	extraConf := &ExtraConfig{EnforceNextProtoSelection: true}
+
+	server := Server(serverConn, config, extraConf)
+	err := server.Handshake()
+	if err == nil || err.Error() != "ALPN negotiation failed. Client offered: [\"proto1\" \"proto2\"]" {
+		t.Fatalf("Expected APLN negotiation to fail, got %s", err)
+	}
+	cErr := <-cErrChan
+	if cErr == nil || !strings.Contains(cErr.Error(), "no application protocol") {
+		t.Fatalf("Expect 'no_application_protocol' error, got %s", cErr)
+	}
+}
+
 func TestHandshakeServerALPNNoMatch(t *testing.T) {
 	config := testConfig.Clone()
 	config.NextProtos = []string{"proto3"}